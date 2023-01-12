9c2b66a49c7c39d638b22e5d88167846c924f78f
FeiGeChuanShu <774074168@qq.com>
2023-01-12 22:48:14 +0800

    update yolov8s-seg. code

--- README.md
+++ README.md
@@ -1,40 +1,13 @@
-# ncnn-android-yolov8
+# ncnn-yolov8 detection and segmentation demo
 
-The yolov8 object detection
+The yolov8 object detection and segmentation
 
-This is a sample ncnn android project, it depends on ncnn library and opencv
-
-https://github.com/Tencent/ncnn
-
-https://github.com/nihui/opencv-mobile
-
-
-## how to build and run
-### step1
-https://github.com/Tencent/ncnn/releases
-
-* Download ncnn-YYYYMMDD-android-vulkan.zip or build ncnn for android yourself
-* Extract ncnn-YYYYMMDD-android-vulkan.zip into **app/src/main/jni** and change the **ncnn_DIR** path to yours in **app/src/main/jni/CMakeLists.txt**
-
-### step2
-https://github.com/nihui/opencv-mobile
-
-* Download opencv-mobile-XYZ-android.zip
-* Extract opencv-mobile-XYZ-android.zip into **app/src/main/jni** and change the **OpenCV_DIR** path to yours in **app/src/main/jni/CMakeLists.txt**
-
-### step3
-* Open this project with Android Studio, build it and enjoy!
-
-## some notes
-* Android ndk camera is used for best efficiency
-* Crash may happen on very old devices for lacking HAL3 camera interface
-* All models are manually modified to accept dynamic input shape
-* Most small models run slower on GPU than on CPU, this is common
-* FPS may be lower in dark environment because of longer camera exposure time
+1.Android yolov8 detection demo
+2.yolov8s-seg.cpp
 
 ## screenshot
-![](screenshot.png)
-
+![](./ncnn-android-yolov8/screenshot.png)
+![](yolov8s-seg.jpg)
 ## Reference：  
 https://github.com/nihui/ncnn-android-nanodet  
 https://github.com/Tencent/ncnn  
---
+++ yolov8-seg.cpp
@@ -0,0 +1,653 @@
+#include "net.h"
+
+#if defined(USE_NCNN_SIMPLEOCV)
+#include "simpleocv.h"
+#else
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#endif
+#include <stdlib.h>
+#include <float.h>
+#include <stdio.h>
+#include <vector>
+static void slice(const ncnn::Mat& in, ncnn::Mat& out, int start, int end, int axis)
+{
+    ncnn::Option opt;
+    opt.num_threads = 4;
+    opt.use_fp16_storage = false;
+    opt.use_packing_layout = false;
+
+    ncnn::Layer* op = ncnn::create_layer("Crop");
+
+    // set param
+    ncnn::ParamDict pd;
+
+    ncnn::Mat axes = ncnn::Mat(1);
+    axes.fill(axis);
+    ncnn::Mat ends = ncnn::Mat(1);
+    ends.fill(end);
+    ncnn::Mat starts = ncnn::Mat(1);
+    starts.fill(start);
+    pd.set(9, starts);// start
+    pd.set(10, ends);// end
+    pd.set(11, axes);//axes
+
+    op->load_param(pd);
+
+    op->create_pipeline(opt);
+
+    // forward
+    op->forward(in, out, opt);
+
+    op->destroy_pipeline(opt);
+
+    delete op;
+}
+static void interp(const ncnn::Mat& in, const float& scale, const int& out_w, const int& out_h, ncnn::Mat& out)
+{
+    ncnn::Option opt;
+    opt.num_threads = 4;
+    opt.use_fp16_storage = false;
+    opt.use_packing_layout = false;
+
+    ncnn::Layer* op = ncnn::create_layer("Interp");
+
+    // set param
+    ncnn::ParamDict pd;
+    pd.set(0, 2);// resize_type
+    pd.set(1, scale);// height_scale
+    pd.set(2, scale);// width_scale
+    pd.set(3, out_h);// height
+    pd.set(4, out_w);// width
+
+    op->load_param(pd);
+
+    op->create_pipeline(opt);
+
+    // forward
+    op->forward(in, out, opt);
+
+    op->destroy_pipeline(opt);
+
+    delete op;
+}
+static void reshape(const ncnn::Mat& in, ncnn::Mat& out, int c, int h, int w, int d)
+{
+    ncnn::Option opt;
+    opt.num_threads = 4;
+    opt.use_fp16_storage = false;
+    opt.use_packing_layout = false;
+
+    ncnn::Layer* op = ncnn::create_layer("Reshape");
+
+    // set param
+    ncnn::ParamDict pd;
+
+    pd.set(0, w);// start
+    pd.set(1, h);// end
+    if (d > 0)
+        pd.set(11, d);//axes
+    pd.set(2, c);//axes
+    op->load_param(pd);
+
+    op->create_pipeline(opt);
+
+    // forward
+    op->forward(in, out, opt);
+
+    op->destroy_pipeline(opt);
+
+    delete op;
+}
+static void sigmoid(ncnn::Mat& bottom)
+{
+    ncnn::Option opt;
+    opt.num_threads = 4;
+    opt.use_fp16_storage = false;
+    opt.use_packing_layout = false;
+
+    ncnn::Layer* op = ncnn::create_layer("Sigmoid");
+
+    op->create_pipeline(opt);
+
+    // forward
+
+    op->forward_inplace(bottom, opt);
+    op->destroy_pipeline(opt);
+
+    delete op;
+}
+static void matmul(const std::vector<ncnn::Mat>& bottom_blobs, ncnn::Mat& top_blob)
+{
+    ncnn::Option opt;
+    opt.num_threads = 2;
+    opt.use_fp16_storage = false;
+    opt.use_packing_layout = false;
+
+    ncnn::Layer* op = ncnn::create_layer("MatMul");
+
+    // set param
+    ncnn::ParamDict pd;
+    pd.set(0, 0);// axis
+
+    op->load_param(pd);
+
+    op->create_pipeline(opt);
+    std::vector<ncnn::Mat> top_blobs(1);
+    op->forward(bottom_blobs, top_blobs, opt);
+    top_blob = top_blobs[0];
+
+    op->destroy_pipeline(opt);
+
+    delete op;
+}
+
+struct Object
+{
+    cv::Rect_<float> rect;
+    int label;
+    float prob;
+    cv::Mat mask;
+    std::vector<float> mask_feat;
+};
+struct GridAndStride
+{
+    int grid0;
+    int grid1;
+    int stride;
+};
+static inline float intersection_area(const Object& a, const Object& b)
+{
+    cv::Rect_<float> inter = a.rect & b.rect;
+    return inter.area();
+}
+
+static void qsort_descent_inplace(std::vector<Object>& faceobjects, int left, int right)
+{
+    int i = left;
+    int j = right;
+    float p = faceobjects[(left + right) / 2].prob;
+
+    while (i <= j)
+    {
+        while (faceobjects[i].prob > p)
+            i++;
+
+        while (faceobjects[j].prob < p)
+            j--;
+
+        if (i <= j)
+        {
+            // swap
+            std::swap(faceobjects[i], faceobjects[j]);
+
+            i++;
+            j--;
+        }
+    }
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            if (left < j) qsort_descent_inplace(faceobjects, left, j);
+        }
+        #pragma omp section
+        {
+            if (i < right) qsort_descent_inplace(faceobjects, i, right);
+        }
+    }
+}
+
+static void qsort_descent_inplace(std::vector<Object>& faceobjects)
+{
+    if (faceobjects.empty())
+        return;
+
+    qsort_descent_inplace(faceobjects, 0, faceobjects.size() - 1);
+}
+
+static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold)
+{
+    picked.clear();
+
+    const int n = faceobjects.size();
+
+    std::vector<float> areas(n);
+    for (int i = 0; i < n; i++)
+    {
+        areas[i] = faceobjects[i].rect.width * faceobjects[i].rect.height;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        const Object& a = faceobjects[i];
+
+        int keep = 1;
+        for (int j = 0; j < (int)picked.size(); j++)
+        {
+            const Object& b = faceobjects[picked[j]];
+
+            // intersection over union
+            float inter_area = intersection_area(a, b);
+            float union_area = areas[i] + areas[picked[j]] - inter_area;
+            // float IoU = inter_area / union_area
+            if (inter_area / union_area > nms_threshold)
+                keep = 0;
+        }
+
+        if (keep)
+            picked.push_back(i);
+    }
+}
+inline float fast_exp(float x)
+{
+    union {
+        uint32_t i;
+        float f;
+    } v{};
+    v.i = (1 << 23) * (1.4426950409 * x + 126.93490512f);
+    return v.f;
+}
+
+inline float sigmoid(float x)
+{
+    return 1.0f / (1.0f + fast_exp(-x));
+}
+static void generate_proposals(std::vector<GridAndStride> grid_strides, const ncnn::Mat& pred, float prob_threshold, std::vector<Object>& objects)
+{
+    const int num_points = grid_strides.size();
+    const int num_class = 80;
+    const int reg_max_1 = 16;
+
+    for (int i = 0; i < num_points; i++)
+    {
+
+            const float* scores = pred.row(i) + 4 * reg_max_1;
+
+            // find label with max score
+            int label = -1;
+            float score = -FLT_MAX;
+            for (int k = 0; k < num_class; k++)
+            {
+                float confidence = sigmoid(scores[k]);
+                if (confidence > score)
+                {
+                    label = k;
+                    score = confidence;
+                }
+            }
+            
+            if (score >= prob_threshold)
+            {
+                ncnn::Mat bbox_pred(reg_max_1, 4, (void*)pred.row(i));
+                {
+                    ncnn::Layer* softmax = ncnn::create_layer("Softmax");
+
+                    ncnn::ParamDict pd;
+                    pd.set(0, 1); // axis
+                    pd.set(1, 1);
+                    softmax->load_param(pd);
+
+                    ncnn::Option opt;
+                    opt.num_threads = 1;
+                    opt.use_packing_layout = false;
+
+                    softmax->create_pipeline(opt);
+
+                    softmax->forward_inplace(bbox_pred, opt);
+
+                    softmax->destroy_pipeline(opt);
+
+                    delete softmax;
+                }
+
+                float pred_ltrb[4];
+                for (int k = 0; k < 4; k++)
+                {
+                    float dis = 0.f;
+                    const float* dis_after_sm = bbox_pred.row(k);
+                    for (int l = 0; l < reg_max_1; l++)
+                    {
+                        dis += l * dis_after_sm[l];
+                    }
+
+                    pred_ltrb[k] = dis * grid_strides[i].stride;
+                }
+
+                float pb_cx = (grid_strides[i].grid0 + 0.5f) * grid_strides[i].stride;
+                float pb_cy = (grid_strides[i].grid1 + 0.5f) * grid_strides[i].stride;
+
+                float x0 = pb_cx - pred_ltrb[0];
+                float y0 = pb_cy - pred_ltrb[1];
+                float x1 = pb_cx + pred_ltrb[2];
+                float y1 = pb_cy + pred_ltrb[3];
+
+                Object obj;
+                obj.rect.x = x0;
+                obj.rect.y = y0;
+                obj.rect.width = x1 - x0;
+                obj.rect.height = y1 - y0;
+                obj.label = label;
+                obj.prob = score;
+                obj.mask_feat.resize(32);
+                std::copy(pred.row(i) + 64 + num_class, pred.row(i) + 64 + num_class + 32, obj.mask_feat.begin());
+                objects.push_back(obj);
+            }
+    }
+}
+static void generate_grids_and_stride(const int target_w, const int target_h, std::vector<int>& strides, std::vector<GridAndStride>& grid_strides)
+{
+    for (int i = 0; i < (int)strides.size(); i++)
+    {
+        int stride = strides[i];
+        int num_grid_w = target_w / stride;
+        int num_grid_h = target_h / stride;
+        for (int g1 = 0; g1 < num_grid_h; g1++)
+        {
+            for (int g0 = 0; g0 < num_grid_w; g0++)
+            {
+                GridAndStride gs;
+                gs.grid0 = g0;
+                gs.grid1 = g1;
+                gs.stride = stride;
+                grid_strides.push_back(gs);
+            }
+        }
+    }
+}
+static void decode_mask(const ncnn::Mat& mask_feat, const int& img_w, const int& img_h,
+    const ncnn::Mat& mask_proto, const ncnn::Mat& in_pad, const int& wpad, const int& hpad,
+    ncnn::Mat& mask_pred_result)
+{
+    ncnn::Mat masks;
+    matmul(std::vector<ncnn::Mat>{mask_feat, mask_proto}, masks);
+    sigmoid(masks);
+    reshape(masks, masks, masks.h, in_pad.h / 4, in_pad.w / 4, 0);
+    slice(masks, mask_pred_result, (wpad / 2)/4, (in_pad.w - wpad / 2)/4, 2);
+    slice(mask_pred_result, mask_pred_result, (hpad / 2)/4, (in_pad.h - hpad / 2)/4, 1);
+    interp(mask_pred_result, 4.0, img_w, img_h, mask_pred_result);
+
+}
+static int detect_yolov8(const cv::Mat& bgr, std::vector<Object>& objects)
+{
+    ncnn::Net yolov8;
+
+    yolov8.load_param("v8s-seg.param");
+    yolov8.load_model("v8s-seg.bin");
+
+    int width = bgr.cols;
+    int height = bgr.rows;
+
+    const int target_size = 640;
+    const float prob_threshold = 0.4f;
+    const float nms_threshold = 0.5f;
+
+    // pad to multiple of 32
+    int w = width;
+    int h = height;
+    float scale = 1.f;
+    if (w > h)
+    {
+        scale = (float)target_size / w;
+        w = target_size;
+        h = h * scale;
+    }
+    else
+    {
+        scale = (float)target_size / h;
+        h = target_size;
+        w = w * scale;
+    }
+
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(bgr.data, ncnn::Mat::PIXEL_BGR2RGB, width, height, w, h);
+
+    // pad to target_size rectangle
+    int wpad = (w + 31) / 32 * 32 - w;
+    int hpad = (h + 31) / 32 * 32 - h;
+    ncnn::Mat in_pad;
+    ncnn::copy_make_border(in, in_pad, hpad / 2, hpad - hpad / 2, wpad / 2, wpad - wpad / 2, ncnn::BORDER_CONSTANT, 0.f);
+
+    const float norm_vals[3] = { 1 / 255.f, 1 / 255.f, 1 / 255.f };
+    in_pad.substract_mean_normalize(0, norm_vals);
+
+
+    ncnn::Extractor ex = yolov8.create_extractor();
+    ex.input("images", in_pad);
+
+    ncnn::Mat out;
+    ex.extract("output", out);
+
+    ncnn::Mat mask_proto;
+    ex.extract("seg", mask_proto);
+
+    std::vector<int> strides = { 8, 16, 32 };
+    std::vector<GridAndStride> grid_strides;
+    generate_grids_and_stride(in_pad.w, in_pad.h, strides, grid_strides);
+
+    std::vector<Object> proposals;
+    std::vector<Object> objects8;
+    generate_proposals(grid_strides, out, prob_threshold, objects8);
+    
+    proposals.insert(proposals.end(), objects8.begin(), objects8.end());
+
+    // sort all proposals by score from highest to lowest
+    qsort_descent_inplace(proposals);
+
+    // apply nms with nms_threshold
+    std::vector<int> picked;
+    nms_sorted_bboxes(proposals, picked, nms_threshold);
+
+    int count = picked.size();
+
+    ncnn::Mat mask_feat = ncnn::Mat(32, count, sizeof(float));
+    for (int i = 0; i < count; i++) {
+        float* mask_feat_ptr = mask_feat.row(i);
+        std::memcpy(mask_feat_ptr, proposals[picked[i]].mask_feat.data(), sizeof(float) * proposals[picked[i]].mask_feat.size());
+    }
+
+    ncnn::Mat mask_pred_result;
+    decode_mask(mask_feat, width, height, mask_proto, in_pad, wpad, hpad, mask_pred_result);
+
+    objects.resize(count);
+    for (int i = 0; i < count; i++)
+    {
+        objects[i] = proposals[picked[i]];
+
+        // adjust offset to original unpadded
+        float x0 = (objects[i].rect.x - (wpad / 2)) / scale;
+        float y0 = (objects[i].rect.y - (hpad / 2)) / scale;
+        float x1 = (objects[i].rect.x + objects[i].rect.width - (wpad / 2)) / scale;
+        float y1 = (objects[i].rect.y + objects[i].rect.height - (hpad / 2)) / scale;
+
+        // clip
+        x0 = std::max(std::min(x0, (float)(width - 1)), 0.f);
+        y0 = std::max(std::min(y0, (float)(height - 1)), 0.f);
+        x1 = std::max(std::min(x1, (float)(width - 1)), 0.f);
+        y1 = std::max(std::min(y1, (float)(height - 1)), 0.f);
+
+        objects[i].rect.x = x0;
+        objects[i].rect.y = y0;
+        objects[i].rect.width = x1 - x0;
+        objects[i].rect.height = y1 - y0;
+
+        objects[i].mask = cv::Mat::zeros(height, width, CV_32FC1);
+        cv::Mat mask = cv::Mat(height, width, CV_32FC1, (float*)mask_pred_result.channel(i));
+        mask(objects[i].rect).copyTo(objects[i].mask(objects[i].rect));
+    }
+
+    return 0;
+}
+
+static void draw_objects(const cv::Mat& bgr, const std::vector<Object>& objects)
+{
+    static const char* class_names[] = {
+        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+        "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+        "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+        "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+        "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+        "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+        "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+        "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+        "hair drier", "toothbrush"
+    };
+    static const unsigned char colors[81][3] = {
+            {56,  0,   255},
+            {226, 255, 0},
+            {0,   94,  255},
+            {0,   37,  255},
+            {0,   255, 94},
+            {255, 226, 0},
+            {0,   18,  255},
+            {255, 151, 0},
+            {170, 0,   255},
+            {0,   255, 56},
+            {255, 0,   75},
+            {0,   75,  255},
+            {0,   255, 169},
+            {255, 0,   207},
+            {75,  255, 0},
+            {207, 0,   255},
+            {37,  0,   255},
+            {0,   207, 255},
+            {94,  0,   255},
+            {0,   255, 113},
+            {255, 18,  0},
+            {255, 0,   56},
+            {18,  0,   255},
+            {0,   255, 226},
+            {170, 255, 0},
+            {255, 0,   245},
+            {151, 255, 0},
+            {132, 255, 0},
+            {75,  0,   255},
+            {151, 0,   255},
+            {0,   151, 255},
+            {132, 0,   255},
+            {0,   255, 245},
+            {255, 132, 0},
+            {226, 0,   255},
+            {255, 37,  0},
+            {207, 255, 0},
+            {0,   255, 207},
+            {94,  255, 0},
+            {0,   226, 255},
+            {56,  255, 0},
+            {255, 94,  0},
+            {255, 113, 0},
+            {0,   132, 255},
+            {255, 0,   132},
+            {255, 170, 0},
+            {255, 0,   188},
+            {113, 255, 0},
+            {245, 0,   255},
+            {113, 0,   255},
+            {255, 188, 0},
+            {0,   113, 255},
+            {255, 0,   0},
+            {0,   56,  255},
+            {255, 0,   113},
+            {0,   255, 188},
+            {255, 0,   94},
+            {255, 0,   18},
+            {18,  255, 0},
+            {0,   255, 132},
+            {0,   188, 255},
+            {0,   245, 255},
+            {0,   169, 255},
+            {37,  255, 0},
+            {255, 0,   151},
+            {188, 0,   255},
+            {0,   255, 37},
+            {0,   255, 0},
+            {255, 0,   170},
+            {255, 0,   37},
+            {255, 75,  0},
+            {0,   0,   255},
+            {255, 207, 0},
+            {255, 0,   226},
+            {255, 245, 0},
+            {188, 255, 0},
+            {0,   255, 18},
+            {0,   255, 75},
+            {0,   255, 151},
+            {255, 56,  0},
+            {245, 255, 0}
+    };
+    cv::Mat image = bgr.clone();
+    int color_index = 0;
+    for (size_t i = 0; i < objects.size(); i++)
+    {
+        const Object& obj = objects[i];
+        const unsigned char* color = colors[color_index % 80];
+        color_index++;
+
+        cv::Scalar cc(color[0], color[1], color[2]);
+
+        fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f x %.2f\n", obj.label, obj.prob,
+                obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height);
+        for (int y = 0; y < image.rows; y++) {
+            uchar* image_ptr = image.ptr(y);
+            const float* mask_ptr = obj.mask.ptr<float>(y);
+            for (int x = 0; x < image.cols; x++) {
+                if (mask_ptr[x] >= 0.5)
+                {
+                    image_ptr[0] = cv::saturate_cast<uchar>(image_ptr[0] * 0.5 + color[2] * 0.5);
+                    image_ptr[1] = cv::saturate_cast<uchar>(image_ptr[1] * 0.5 + color[1] * 0.5);
+                    image_ptr[2] = cv::saturate_cast<uchar>(image_ptr[2] * 0.5 + color[0] * 0.5);
+                }
+                image_ptr += 3;
+            }
+        }
+        cv::rectangle(image, obj.rect, cc, 2);
+
+        char text[256];
+        sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
+
+        int baseLine = 0;
+        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+
+        int x = obj.rect.x;
+        int y = obj.rect.y - label_size.height - baseLine;
+        if (y < 0)
+            y = 0;
+        if (x + label_size.width > image.cols)
+            x = image.cols - label_size.width;
+
+        cv::rectangle(image, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+                      cv::Scalar(255, 255, 255), -1);
+
+        cv::putText(image, text, cv::Point(x, y + label_size.height),
+                    cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+    }
+    cv::imwrite("i.jpg", image);
+    cv::imshow("image", image);
+    cv::waitKey(0);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        return -1;
+    }
+
+    const char* imagepath = argv[1];
+
+    cv::Mat m = cv::imread(imagepath, 1);
+    if (m.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", imagepath);
+        return -1;
+    }
+
+    std::vector<Object> objects;
+    detect_yolov8(m, objects);
+
+    draw_objects(m, objects);
+
+    return 0;
+}
